d197af7d03afce080efcd4e3e7d7551eea194dc4
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-10 14:30:21 +0200

    Création des fonctions pour obtenir la liste des allumettes, et celle pour afficher de façon simple

--- arbitre_jeu_nim.py
+++ arbitre_jeu_nim.py
@@ -6,3 +6,17 @@ Jeu de Nim
 Séverine Hori Maitrehut
 """
 
+def get_list_matches():
+    list_matches = []
+    for i in range(0,21):
+        list_matches.append(0)
+    return list_matches
+
+def print_matches():
+    for indice, valeur in enumerate(list_matches):
+        print(f"tab[{indice}] = {valeur}")
+
+if __name__ == '__main__':
+    list_matches = get_list_matches()
+
+    print_matches()
