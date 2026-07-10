2e261f4336f74d6e46bbeea1a1b928a52b9d9e42
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-10 14:58:24 +0200

    Correction de la fonction d'affichage, et ajout de la fonction take_match

--- arbitre_jeu_nim.py
+++ arbitre_jeu_nim.py
@@ -12,17 +12,17 @@ def get_list_matches():
         list_matches.append(0)
     return list_matches
 
-def print_matches(tab_matches):
-    print_matches_string = "Allumettes disponibles : \n"
-    for indice, valeur in enumerate(tab_matches):
+def print_matches(list_matches):
+    print_matches_string = "Allumettes disponibles : \n - "
+    for indice, valeur in enumerate(list_matches):
         if (valeur == 0):
             print_matches_string += str(indice+1) + " - "
-        print(print_matches_string)
+    print(print_matches_string)
+
+def take_match(list_matches, num_player, num_match):
+    list_matches[num_match-1] = num_player
 
 
 if __name__ == '__main__':
-
-
-
     list_matches = get_list_matches()
     print_matches(list_matches)
