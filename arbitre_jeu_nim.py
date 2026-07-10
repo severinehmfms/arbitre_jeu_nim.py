9884e6e773eb83ee5b9c91da666e0e2116003ff8
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-10 14:37:45 +0200

    Mise à jour de la fonction d'affichage

--- arbitre_jeu_nim.py
+++ arbitre_jeu_nim.py
@@ -12,11 +12,17 @@ def get_list_matches():
         list_matches.append(0)
     return list_matches
 
-def print_matches():
-    for indice, valeur in enumerate(list_matches):
-        print(f"tab[{indice}] = {valeur}")
+def print_matches(tab_matches):
+    print_matches_string = "Allumettes disponibles : \n"
+    for indice, valeur in enumerate(tab_matches):
+        if (valeur == 0):
+            print_matches_string += str(indice+1) + " - "
+        print(print_matches_string)
+
 
 if __name__ == '__main__':
-    list_matches = get_list_matches()
 
-    print_matches()
+
+
+    list_matches = get_list_matches()
+    print_matches(list_matches)
