b5f9a91a4205ce7d1f8538f95d36a732717787e4
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-13 09:28:53 +0200

    Le jeu fonctionne

--- arbitre_jeu_nim.py
+++ arbitre_jeu_nim.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
+
 """
 Jeu de Nim
 Séverine Hori Maitrehut
@@ -13,71 +15,114 @@ la valeur est de 2 si l'allumette a été enlevée par le joueur 2
 La liste des joueurs est aussi un tableau simple avec comme valeur le nom du joueur
 """
 
+
 def init_list_matches():
     """ Fonction qui initialise la liste des allumettes, toutes disponibles au début du jeu"""
     list_matches = []
-    for i in range(0,21):
+    for _ in range(0, 21):
         list_matches.append(0)
     return list_matches
+
 
 def get_nb_availables_matches(list_matches):
     """ Fonction qui renvoie le nombre d'allumettes disponibles"""
     return list_matches.count(0)
 
-def print_matches(list_matches):
-    """Fonction qui affiche la liste des allumettes passée en paramètre - dans notre cas la liste des allumettes disponibles"""
-    print_matches_string = "Allumettes disponibles : \n - "
-    for indice, valeur in enumerate(list_matches):
-        if (valeur == 0):
-            print_matches_string += str(indice+1) + " - "
-    print(print_matches_string)
 
-def take_match(list_matches, num_player, num_match):
-    """Fonction qui simule l'enlèvement d'une allumette par un joueur : on met à 1 la case si c'est le premier joueur qui l'a enlevée, 2 sinon"""
-    list_matches[int(num_match)-1] = int(num_player)
-
-# TODO
-def is_entry_player_ok(saisie):
-    """Fonction qui teste si les prénoms des utilisateurs sont corrects"""
-    cleaned_saisie = saisie.strip()
-    if not cleaned_saisie.isalpha():
+def is_available_match(num_match, list_matches):
+    """ Fonction qui renvoie True si le numéro de l'allumette est disponible """
+    if list_matches[num_match-1] != 0:
         return False
     return True
 
-# TODO
-def is_entry_matches_ok(saisie):
-    """entre 1 et 4 numéros d'allumettes,
-    espacés par des espaces. Chaque numéro d'allumette doit être entre 1 et 21, et doit faire partie des allumettes restantes"""
+
+def print_matches(list_matches):
+    """Fonction qui affiche la liste des allumettes passée en paramètre (liste des allumettes disponibles)"""
+    print_matches_string = "Allumettes disponibles : \n - "
+    for indice, valeur in enumerate(list_matches):
+        if valeur == 0:
+            print_matches_string += str(indice+1) + " - "
+    print(print_matches_string)
+
+
+def take_match(list_matches, num_player, num_match):
+    """Fonction qui simule l'enlèvement d'une allumette par un joueur :
+    on met à 1 la case si c'est le premier joueur qui l'a enlevée, 2 sinon"""
+    list_matches[int(num_match)-1] = int(num_player)
+
+
+def is_entry_player_ok(saisie):
+    """Fonction qui vérifie si la saisie du prénom du joueur est correcte"""
+    cleaned_saisie = saisie.strip()
+    if " " in cleaned_saisie or not cleaned_saisie.isalpha():
+        return False
     return True
 
-# TODO Numérique un ou deux obligatoirement.
+
+def is_entry_matches_ok(saisie, list_matches):
+    """Fonction qui vérifie si la saisie du joueur concernant les numéros des allumettes à enlever est correcte"""
+    """Saisie attendue : entre 1 et 4 numéros d'allumettes, espacés par des espaces. 
+       Chaque numéro d'allumette doit être entre 1 et 21, et doit faire partie des allumettes restant disponibles"""
+    cleaned_saisie = saisie.strip()
+
+    # Expression régulière pour la forme déjà de la saisie (aide chatgpt pour cette expression régulière)
+    pattern = r"^(?:[1-9]|1\d|2[01])(?: (?:[1-9]|1\d|2[01])){0,3}$"
+    if not re.fullmatch(pattern, cleaned_saisie):
+        return False
+
+    elements = cleaned_saisie.split()
+    for element in elements:
+        # On vérifie si le numéro de l'allumette est bien entre 1 et 21
+        if not (1 <= int(element) <= 21):
+            return False
+
+        # On vérifie si cette allumette fait partie des allumettes disponibles
+        if not is_available_match(int(element.strip()), list_matches):
+            print(f"Vous avez entré un numéro d'allumette non disponible : {element.strip()}")
+            return False
+    return True
+
+
 def is_entry_starts_ok(saisie):
-
+    """ Fonction qui vérifie la saisie du joueur pour savoir qui joue en premier
+        Valeur attendue : Numérique, 1 ou 2 obligatoirement
+    """
+    cleaned_saisie = saisie.strip()
+    if not cleaned_saisie.isdigit():
+        return False
+    if int(cleaned_saisie) != 1 and int(cleaned_saisie) != 2:
+        return False
     return True
 
-def get_matches_input(num_player):
-    """Fonction qui demande à l'utilisateur de saisir les allumettes qu'il souhaite enlever (entre 1 et 4 numéros d'allumettes,
-    espacés par des espaces. Chaque numéro d'allumette doit être entre 1 et 21, et doit faire partie des allumettes restantes"""
+
+def get_matches_input(list_matches):
+    """Fonction qui demande au joueur de saisir les allumettes à enlever (entre 1 et 4 numéros d'allumettes,
+    espacés par des espaces. Chaque numéro doit être entre 1 et 21, et doit faire partie des allumettes restantes"""
     input_matches = input("Saisir les allumettes à retirer (num de l'allumette, séparés par un espace) : ")
-    while not is_entry_matches_ok(input_matches.strip()):
+    while not is_entry_matches_ok(input_matches, list_matches):
         input_matches = input("Saisie incorrecte. Merci de recommencer : ")
     return input_matches
 
+
 def get_players_input(num_player):
-    input_player = input(f"Saisir le nom de l'utilisateur {num_player} : ")
-    while not is_entry_player_ok(input_player.strip()):
+    """Fonction qui demande au joueur de saisir son nom"""
+    input_player = input(f"Saisir le nom du joueur {num_player} : ")
+    while not is_entry_player_ok(input_player):
         input_player = input("Saisie incorrecte. Merci de recommencer : ")
     return input_player
 
+
 def get_who_starts_input(player1, player2):
-    starts_input = input(f"Qui commence ? ({player1} tapez 1, ou {player2} tapez 2 : ")
+    """Fonction qui demande au joueur quel joueur va commencer la partie"""
+    starts_input = input(f"Qui commence ? ({player1} tapez 1, ou {player2} tapez 2) : ")
     while not is_entry_starts_ok(starts_input):
         starts_input = input("Saisie incorrecte. Merci de recommencer : ")
-    #On enlève 1 pour s'adapter à notre tableau
+    # On enlève 1 pour s'adapter à notre tableau
     return int(starts_input)
 
+
 if __name__ == '__main__':
-    print("Jeu de Nim")
+    print("******************** Jeu de Nim ***********************")
 
     list_matches = init_list_matches()
     print_matches(list_matches)
@@ -90,16 +135,16 @@ if __name__ == '__main__':
 
     while get_nb_availables_matches(list_matches) > 0:
         print(f"Au tour de {players[int(who_plays)-1]} de jouer.")
-        matches_plays = get_matches_input(who_plays)
+        matches_plays = get_matches_input(list_matches)
         list_matches_played = matches_plays.split()
-        # TODO On récupère les allumettes jouées par le joueur et on les traite
+        # On récupère les allumettes jouées par le joueur et on les traite
         for match in list_matches_played:
             print(f"Le joueur {players[int(who_plays)-1]} a choisi l'allumette numéro {match}")
             take_match(list_matches, who_plays, match)
-        if get_nb_availables_matches(list_matches)!=0:
-            #On change de joueur
+        if get_nb_availables_matches(list_matches) > 0:
+            # On change de joueur
             who_plays = 2 if who_plays == 1 else 1
-        #On affiche les allumettes
+        # On affiche les allumettes
         print_matches(list_matches)
 
     print(f"{players[int(who_plays)-1]} a enlevé la dernière allumette et a perdu !")
